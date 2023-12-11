f113eea4e95c0946e27fc0f59154439c7783555d
ArifLaode <152675401+ArifLaode@users.noreply.github.com>
2023-12-11 13:22:44 +0800

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+
+const db = mysql.createConnection(process.env.DB_SERVER);
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+module.exports = db;
